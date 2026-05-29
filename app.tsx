024125953668a75dd4d86ae03b07cffe421f45dc
jonatasbarra <email.jonatas@gmail.com>
2026-05-29 15:31:57 -0400

    feat: floating notification popups with auto-dismiss (D6)

--- app.tsx
+++ app.tsx
@@ -1,11 +1,13 @@
 import app from "ags/gtk4/app"
 import Bar from "./widgets/bar/Bar"
 import ControlCenter from "./widgets/controlcenter/ControlCenter"
+import Notifications from "./widgets/notifications/Notifications"
 
 app.start({
   css: `${SRC}/style.css`,
   main() {
     app.get_monitors().forEach(Bar)
     ControlCenter()
+    Notifications()
   },
 })
